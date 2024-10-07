9178bdf5eb173d57e31f3c71f787f80f59bc3156
jawaluke <jawaharlakshmanan6@gmail.com>
2024-10-07 23:27:15 +0530

    init

---
+++ src/main/java/org/learn/SpringBootWorkAroundBranch/config/PgDataSourceConfig.java
@@ -0,0 +1,21 @@
+package org.learn.SpringBootWorkAroundBranch.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PgDataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(
+            prefix = "spring.datasource.pg"
+    )
+    public DataSource getDataSource() {
+        return new DataSourceProperties().initializeDataSourceBuilder().build();
+    }
+
+}
